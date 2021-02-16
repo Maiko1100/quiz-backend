af95fbd5e027dd5de9cb581df407f005ff633404
maiko kuipers <>
2021-02-16 18:33:51 +0100

    added cors

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,14 @@ const express = require("express");
 import * as bodyParser from "body-parser";
 import initQuizController from "./controllers/QuizController";
 import mongoose from "mongoose";
-
+import cors from 'cors'
+var corsOptions = {
+  origin: "*"
+};
 const app = express();
 
 app.use(bodyParser.json({ limit: "5mb" }));
-
+app.use(cors(corsOptions));
 initQuizController(app);
 
 async function startServer() {
