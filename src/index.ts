869fe39e87f4894b3bef9f5eb67f72539a26f885
maiko kuipers <>
2021-02-17 22:22:41 +0100

    added headers

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,14 @@ import { getConnectionStringAndOption } from "./utils/getDatabaseConnectionSetti
 // };
 const app = express();
 app.use(cors());
+app.use(function (req: any, res: any, next: any) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(bodyParser.json({ limit: "5mb" }));
 
 initQuizController(app);
