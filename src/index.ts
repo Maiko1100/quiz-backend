7627eb36d3ca8870cc6db31ea86c67c5983a2fa0
maiko kuipers <>
2021-02-17 22:59:25 +0100

    no message

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ const app = express();
 app.use(function (req: any, res: any, next: any) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
-    "Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS"
+    "Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS"
   );
   res.header(
     "Access-Control-Allow-Headers",
