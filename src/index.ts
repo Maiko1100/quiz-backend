dff60ba5ecd64f650d08e8892b91f06266d9cc97
maiko kuipers <>
2021-02-17 22:28:01 +0100

    removed cors and added headers

--- src/index.ts
+++ src/index.ts
@@ -11,14 +11,10 @@ import { getConnectionStringAndOption } from "./utils/getDatabaseConnectionSetti
 //   allowedHeaders:['cross-origin','content-type']
 // };
 const app = express();
-app.use(cors());
-app.use(function (req: any, res: any, next: any) {
+// app.use(cors());
+app.use(function(req:any, res:any, next:any) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "OPTIONS,GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 app.use(bodyParser.json({ limit: "5mb" }));
